1a8e54075e970e78f039cfe0cd16d25ba1060ae0
James Adney <jfadney@gmail.com>
2013-05-14 14:52:47 -0700

    just ticketee from now

--- ch1/things_i_bought/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ThingsIBought::Application.config.secret_token = '9175f012d395e79d8a1ee2c319c99139fd3cb4854dfd7e51277ff29f659f9fa07ddf6267bc6bcb8e35144b6f534ae21992851eb7d363cbcd52b591a09379565c'
