1a7ad40b07f33548613636f3af88efaa5e65c82b
James Adney <jfadney@gmail.com>
2013-05-18 20:34:12 -0700

    Lock down specific projects controller actions for admins only

--- spec/integration/hidden_links_spec.rb
+++ spec/integration/hidden_links_spec.rb
@@ -3,11 +3,22 @@ require 'spec_helper'
 feature 'hidden links' do
   let(:user) { Factory(:confirmed_user) }
   let(:admin) { Factory(:admin_user) }
+  let(:project) { Factory(:project) }
 
   context 'anonymous users' do
     scenario 'cannot see the New Project link' do
       visit '/'
       assert_no_link_for "New Project"
+    end
+
+    scenario 'cannot see the Edit Project link' do
+      visit project_path(project)
+      assert_no_link_for "Edit Project"
+    end
+
+    scenario 'cannot see the Delete Project link' do
+      visit project_path(project)
+      assert_no_link_for "Delete Project"
     end
   end
 
@@ -15,15 +26,37 @@ feature 'hidden links' do
     before { sign_in_as!(user) }
     scenario 'cannot see the New Project link' do
       visit '/'
-      assert_no_link_for 'New Project'
+      assert_no_link_for "New Project"
+    end
+
+    scenario 'cannot see the Edit Project link' do
+      visit project_path(project)
+      assert_no_link_for "Edit Project"
+    end
+
+    scenario 'cannot see the Delete Project link' do
+      visit project_path(project)
+      assert_no_link_for "Delete Project"
     end
   end
 
   context 'admin users' do
     before { sign_in_as!(admin) }
+
     scenario 'can see the New Project link' do
       visit '/'
       assert_link_for "New Project"
     end
+
+    scenario 'can see the Edit Project link' do
+      visit project_path(project)
+      assert_link_for "Edit Project"
+    end
+
+    scenario 'can see the Delete Project link' do
+      visit project_path(project)
+      assert_link_for "Delete Project"
+    end
+
   end
 end
